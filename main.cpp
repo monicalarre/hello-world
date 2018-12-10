714a95f2d8f40885d91e199faa5901e4881d1e24
monicalarre <45774259+monicalarre@users.noreply.github.com>
2018-12-10 17:10:48 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int a=10, b=5, c=4;
+    if(a>b)
+        cout<<"a es mayor"<<endl;
+    else
+        cout<<"b es mayor"<<endl;
+    return 0;
+}
